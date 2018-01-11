e4d3318e802f1e8a25082dca022ea7f35dbdc960
Harshini A <harshinidurai98@gmail.com>
2018-01-11 10:37:48 +0000

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int num,digit,temp,rev=0;
+printf("enter the no");
+scanf("%d",&num);
+temp=num;
+while(num!=0)
+{
+digit=num%10;
+rev=rev*10+digit;
+temp=temp/10;
+}
+if(num==rev)
+printf("the number %d is palindrome",num);
+return 0;
+}
