4d3c59c65e8fa21f47f9d1335f8eaceea44e7cb0
Harshini A <harshinidurai98@gmail.com>
2018-01-11 10:17:25 +0000

    Update armstrong.c

--- armstrong.c
+++ armstrong.c
@@ -3,14 +3,14 @@ int main()
 {
 int num,temp,digit,sum=0;
 scanf("%d",&num);
-while(num>1)
-{
 temp=num;
-digit=num%10;
+while(temp!=0)
+{
+digit=temp%10;
 sum=sum+digit*digit*digit;
-num=num/10;
+temp=temp/10;
 }
-if(sum==num)
+if(num==sum)
 printf("Armstrong no");
 else 
 printf("not armstrong  no");
